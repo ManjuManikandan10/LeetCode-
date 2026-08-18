f8874833e629321554599debcd22271ea06ebc80
ManjuManikandan10 <manjum.mcs25@rathinam.in>
2026-08-18 10:18:57 +0530

    Time: 1 ms (99.15%), Space: 42.1 MB (97.94%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean consecutiveSetBits(int n) {
+        int count=0;
+        while(n>1){
+            if((n&3)==3){
+                count++;
+            }
+            if(count>1){
+                return false;
+            }
+            n>>=1;
+        }
+        return count==1;
+    }
+}
