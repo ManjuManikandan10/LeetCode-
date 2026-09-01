1091db6b8a5127ca3ccde74c3af477573fe12a20
ManjuManikandan10 <manjum.mcs25@rathinam.in>
2026-09-01 18:55:47 +0530

    Time: 10 ms (93.89%), Space: 44 MB (77.44%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,29 @@
+class Foo {
+
+    CountDownLatch cd1;
+    CountDownLatch cd2;
+
+    public Foo() {
+        cd1 = new CountDownLatch(1);
+        cd2 = new CountDownLatch(1);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        cd1.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        cd1.await();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        cd2.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        cd2.await();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
