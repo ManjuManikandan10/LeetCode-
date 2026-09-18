76da051978a05f2680d11f59e8352df207907d5f
ManjuManikandan10 <manjum.mcs25@rathinam.in>
2026-09-18 14:28:02 +0530

    Time: 0 ms (100%), Space: 43.2 MB (59.34%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) 
+    {
+        if (root == null) return false;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean foundX = false;
+            boolean foundY = false;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.val == x) foundX = true;
+                if (node.val == y) foundY = true;
+
+                // check if siblings
+                if (node.left != null && node.right != null) {
+                    if ((node.left.val == x && node.right.val == y) ||
+                        (node.left.val == y && node.right.val == x)) {
+                        return false;
+                    }
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            // after one level
+            if (foundX && foundY) return true;
+            if (foundX || foundY) return false;
+        }
+
+        return false;
+
+    }
+}
